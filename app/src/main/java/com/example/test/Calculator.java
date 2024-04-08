01dc0b45f8b93e42e1bed72126e7c921765b3101
Jandel Macabecha <jandel.macabecha88@gmail.com>
2024-04-08 18:31:17 +0800

    calculator completed

--- app/src/main/java/com/example/test/Calculator.java
+++ app/src/main/java/com/example/test/Calculator.java
@@ -9,13 +9,11 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.EventListener;
+import java.util.Iterator;
 import java.util.Stack;
 
 public class Calculator extends AppCompatActivity {
-    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btnDivide,btnAdd,btnSubtract,btnMultiply,btnEquals,btnDot;
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btnDivide,btnAdd,btnSubtract,btnMultiply,btnEquals,btnDot,btnBackSpace,btnClear;
     TextView equationTV, resultTV;
     boolean dotted = false;
 
@@ -42,7 +40,9 @@ public class Calculator extends AppCompatActivity {
         btnDot = findViewById(R.id.btnDot);
         equationTV = findViewById(R.id.equationTV);
         resultTV = findViewById(R.id.resultTV);
-
+        btnBackSpace = findViewById(R.id.btnBksp);
+        btnClear = findViewById(R.id.btnC3);
+        dotted = false;
         btn1.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
@@ -158,7 +158,7 @@ public class Calculator extends AppCompatActivity {
                     } else if (!isNumeric(equationTV.getText().charAt(equationTV.length()-1)) && equationTV.getText().charAt(equationTV.length()-1) != '+') {
                         equationTV.setText(equationTV.getText().toString().substring(0,equationTV.length()-1)+"+");
                     }else{
-                        evaluate(equationTV.getText().toString());
+                        evaluate();
                     }
                 }
             }
@@ -177,7 +177,7 @@ public class Calculator extends AppCompatActivity {
                     } else if (!isNumeric(equationTV.getText().charAt(equationTV.length()-1)) && equationTV.getText().charAt(equationTV.length()-1) != '-') {
                         equationTV.setText(equationTV.getText().toString().substring(0,equationTV.length()-1)+"-");
                     }else{
-                        evaluate(equationTV.getText().toString());
+                        evaluate();
                     }
                 }
             }
@@ -196,7 +196,7 @@ public class Calculator extends AppCompatActivity {
                     } else if (!isNumeric(equationTV.getText().charAt(equationTV.length()-1)) && equationTV.getText().charAt(equationTV.length()-1) != '*') {
                         equationTV.setText(equationTV.getText().toString().substring(0,equationTV.length()-1)+"*");
                     }else{
-                        evaluate(equationTV.getText().toString());
+                        evaluate();
                     }
                 }
             }
@@ -216,7 +216,7 @@ public class Calculator extends AppCompatActivity {
                     } else if (!isNumeric(equationTV.getText().charAt(equationTV.length()-1)) && equationTV.getText().charAt(equationTV.length()-1) != '/') {
                         equationTV.setText(equationTV.getText().toString().substring(0,equationTV.length()-1)+"/");
                     }else{
-                        evaluate(equationTV.getText().toString());
+                        evaluate();
                     }
                 }
             }
@@ -225,70 +225,93 @@ public class Calculator extends AppCompatActivity {
         btnEquals.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sortEquation(equationTV.getText().toString());
+                evaluateBODMAS();
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultTV.setText("");
+                equationTV.setText("");
+            }
+        });
+
+        btnBackSpace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                equationTV.setText(equationTV.getText().toString().substring(0,equationTV.length()-1));
             }
         });
 
     }
-    @SuppressLint("SetTextI18n")
-    private void evaluate(String equation){
-        StringBuilder operand1 = new StringBuilder();
-        StringBuilder operand2 = new StringBuilder();
-        char operator = 0;
-        float res;
-        if(resultTV.getText().toString().isEmpty()){
-            res = 0;
-        }else{
-            res = Float.parseFloat(resultTV.getText().toString());
+
+
+    private void evaluate(){
+        String equation = equationTV.getText().toString();
+        Stack<Object> foo = new Stack<>();
+        if(!isNumeric(equation.charAt(equation.length()-1))){
+            equation = equation.substring(0,equation.length()-1);
         }
-        System.out.println("Fresh Load res = "+res);
-//        if(!isNumeric(equation.charAt(equation.length()-1))){
-//            equation = equation.substring(0,equation.length()-1);
-//        }
-        char[] foo = equation.toCharArray();
-        System.out.println("equation: "+equation+" equation len: "+equation.length()+" foo len: "+foo.length);
-        for(int i = 0; i <foo.length;i++){
-            if(isNumeric(foo[i])){
-                if (operator == 0){
-                    operand1.append(foo[i]);
+        for(int i = 0; i<equation.length();i++){
+            if(foo.isEmpty()){
+                foo.push(equation.charAt(i));
+            }else if(foo.peek() instanceof Character){
+                System.out.println("YESSSSSSSS"+foo.peek());
+                if(isNumeric((Character) foo.peek())){
+                    System.out.println("I will push "+foo.peek());
+                    foo.push(equation.charAt(i));
                 }else{
-                    operand2.append(foo[i]);
+                    StringBuilder num1 = new StringBuilder();
+                    StringBuilder num2 = new StringBuilder();
+                    char operator = (char) foo.pop();
+                    if(foo.peek() instanceof Float){
+                        num1.append(foo.pop());
+                    }else{
+                        while(!foo.isEmpty()){
+                            num1.append(foo.pop());
+                        }
+                        num1.reverse();
+                    }
+                    for(int j = i;j<equation.length();j++){
+                        if(isNumeric(equation.charAt(j))){
+                            num2.append(equation.charAt(j));
+                            i++;
+                        }else{
+                            i--;
+                            break;
+                        }
+                    }
+                    foo.push(calculateSequentially(operator,Float.parseFloat(String.valueOf(num1)),Float.parseFloat(String.valueOf(num2))));
+                    System.out.println("I pushed "+foo.peek()+" into the stack");
                 }
             }else{
-                if(operator == 0){
-                    operator = foo[i];
-                }else{
-                    if(res == 0){
-                        System.out.println("Operand1: "+operand1+" "+operator+" Operand2: "+operand2);
-                        res = calculate(Float.parseFloat(String.valueOf(operand1)),Float.parseFloat(String.valueOf(operand2)),operator);
-                        operator = 0;
-                        operand2.delete(0,operand2.length());
-                    }else{
-                        System.out.println("Res: "+res+" "+operator+" Operand2: "+operand2);
-                        res = calculate(res,Float.parseFloat(String.valueOf(operand2)),operator);
-                        operator = 0;
-                        operand2.delete(0,operand2.length());
-                    }
-                    operator = foo[i];
-                }
+                foo.push(equation.charAt(i));
             }
         }
-//        res = calculate(Float.parseFloat(String.valueOf(operand1)),Float.parseFloat(String.valueOf(operand2)),operator);
-//        System.out.println("Op1: "+operand1+""+operator+"Op2: "+operand2);
-        resultTV.setText(String.valueOf(res));
+        StringBuffer res = new StringBuffer();
+        while(!foo.isEmpty()){
+            res.append(foo.pop());
+        }
+        resultTV.setText(res);
+        res.setLength(0);
+        foo.clear();
     }
 
-    private float calculate(float num1, float num2, Character operator){
+    private float calculateSequentially(Character operator, float num1, float num2){
         switch (operator){
             case '+':
                 float val = num1 + num2;
-                System.out.println("I returned: "+val);
+                System.out.println("Sum of "+num1+" and "+num2+": "+(num1+num2));
                 return num1+num2;
             case '-':
+                System.out.println("Difference of "+num1+" and "+num2+": "+(num1-num2));
                 return num1-num2;
             case '*':
+                System.out.println("Product of "+num1+" and "+num2+": "+(num1*num2));
                 return num1*num2;
             case '/':
+                System.out.println("Quotient of "+num1+" and "+num2+": "+(num1/num2));
                 return num1/num2;
             default:
                 System.out.println("I returned: 0 as default");
@@ -310,20 +333,96 @@ public class Calculator extends AppCompatActivity {
         }
     }
 
-    private void sortEquation(String equation){
-        StringBuilder op1 = new StringBuilder();
-        StringBuilder op2 = new StringBuilder();
-        float res;
-        if(!isNumeric(equation.charAt(equation.length()-1))){
-            equation = equation.substring(0,equation.length()-1);
+    private void evaluateBODMAS(){
+        String equation = equationTV.getText().toString();
+        Stack<Object> foo = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        //Convert to postfix expression
+        for(int i = 0;i<equation.length();i++){
+            if(!isNumeric(equation.charAt(i))){
+                if(foo.isEmpty()){
+                    System.out.println("I will push "+equation.charAt(i)+" because stack is empty");
+                    foo.push(equation.charAt(i));
+                } else if (getPrescedence((Character) foo.peek()) >= getPrescedence(equation.charAt(i))) {
+                    while(!foo.isEmpty() && getPrescedence((Character) foo.peek()) >= getPrescedence(equation.charAt(i))){
+                        System.out.println("I will pop "+equation.charAt(i) +" because of prescedence");
+                        sb.append(foo.pop());
+                    }
+                    System.out.println("I will push "+ equation.charAt(i) + " after popping due to prescedence");
+                    foo.push(equation.charAt(i));
+                }else{
+                    foo.push(equation.charAt(i));
+                }
+            }else{
+                System.out.println("I will append "+equation.charAt(i));
+                sb.append(equation.charAt(i));
+            }
         }
-        ArrayList<Character> foo = new ArrayList<Character>();
-        for (Character character : equation.toCharArray()) {
-            foo.add(character);
-        }
-        System.out.println(foo);
 
+        while(!foo.isEmpty()){
+            sb.append(foo.pop());
+        }
+        System.out.println("Postfix Expression: "+sb);
+
+        //Evaluate the postfix expression sb
+        for(int i=0;i<sb.length();i++){
+            if(i!=sb.length()-1){
+                if(isNumeric(sb.charAt(i))){
+                    //if numeric then push
+                    System.out.println("I will push "+sb.charAt(i)+" because its numeric");
+                    foo.push(sb.charAt(i));
+                }else{
+                    StringBuilder num1 = new StringBuilder();
+                    StringBuilder num2 = new StringBuilder();
+                    System.out.println("I will pop "+foo.peek()+" because im performing a calculation");
+                    num2.append(foo.pop());
+                    System.out.println("I will pop "+foo.peek()+" because im performing a calculation");
+                    num1.append(foo.pop());
+                    foo.push(calculateSequentially(sb.charAt(i),Float.parseFloat(String.valueOf(num1)),Float.parseFloat(String.valueOf(num2))));
+                }
+            }else{
+                StringBuilder num1 = new StringBuilder();
+                StringBuilder num2 = new StringBuilder();
+                System.out.println("I will pop "+foo.peek()+" because im performing a calculation");
+                num2.append(foo.pop());
+                if(foo.size()>1){
+                    while(!foo.isEmpty()){
+                        if((foo.peek() instanceof Float)){
+                            float myF = (Float) foo.pop();
+                            int temp = (int) myF;
+                            foo.push(temp);
+                        }
+                        System.out.println("I will pop "+foo.peek()+" because im performing a calculation");
+                        num1.append(foo.pop());
+                        num1.reverse();
+                    }
+                }else{
+                    num1.append(foo.pop());
+                }
+                System.out.println("Reversed num1 "+num1);
+                resultTV.setText(String.valueOf(calculateSequentially(sb.charAt(i),Float.parseFloat(String.valueOf(num1)),Float.parseFloat(String.valueOf(num2)))));
+            }
+        }
+
+
+//        resultTV.setText(sb.toString());
 
     }
+
+    private int getPrescedence(char c){
+        switch (c){
+            case '/':
+            case '*':
+                return 2;
+            case '+':
+            case '-':
+                return 1;
+            default:
+                return 0;
+        }
+    }
+
+    // Function to return precedence of operators
+
 
 }
